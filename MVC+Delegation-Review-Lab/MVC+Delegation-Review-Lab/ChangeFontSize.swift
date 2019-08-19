b3ecdcb4398b65a4b1d042b3c5bc46a7f182c974
aglegaspi <lex83nyc@gmail.com>
2019-08-19 18:47:50 -0400

    created new Protocol file ChangeFontSize

---
+++ MVC+Delegation-Review-Lab/MVC+Delegation-Review-Lab/ChangeFontSize.swift
@@ -0,0 +1,14 @@
+//
+//  changeFontSize.swift
+//  MVC+Delegation-Review-Lab
+//
+//  Created by Alexander George Legaspi on 8/19/19.
+//  Copyright © 2019 Benjamin Stone. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol changeFontSize {
+    func getFontSize(value: CGFloat)
+}
