210c7ed733bd667f1a625906bb0ee8d5a531bc21
aglegaspi <lex83nyc@gmail.com>
2019-08-19 18:51:35 -0400

    conformed View Controller to ChangeFontSize protocol and updated delegate tasks

--- MVC+Delegation-Review-Lab/MVC+Delegation-Review-Lab/ViewController.swift
+++ MVC+Delegation-Review-Lab/MVC+Delegation-Review-Lab/ViewController.swift
@@ -8,18 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ChangeFontSize {
     
     let movies = Movie.allMovies
+    var currentSize: CGFloat = 17.0
 
     @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.delegate = self
         tableView.dataSource = self
+
     }
     
+    func getFontSize(value: CGFloat) {
+        currentSize = value
+        tableView.reloadData()
+    }
 
 }
 
@@ -32,14 +40,23 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         if let cell = tableView.dequeueReusableCell(withIdentifier: "moviecell") {
+            
             cell.textLabel?.text = movies[indexPath.row].name
             cell.detailTextLabel?.text = String(movies[indexPath.row].year)
+            
+            cell.textLabel?.font = cell.textLabel?.font.withSize(currentSize)
+            cell.detailTextLabel?.font = cell.detailTextLabel?.font.withSize(currentSize)
+            
             return cell
         }
         
         return UITableViewCell()
-        
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? SettingsViewController {
+            destination.delegate = self
+        }
+    }
     
 }
